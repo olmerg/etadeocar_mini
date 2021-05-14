017c3110eb38a2d6854e3a1250b2433d34e4aa81
MarcosVillaquira <marcosf.villaquiran@utadeo.edu.co>
2021-05-14 14:22:52 -0500

    Mejorado README, codigo funcionando

--- README.md
+++ README.md
@@ -1,20 +1,93 @@
 # Tadeo Car
 ## Robótica industrial
-Con este proyecto se busca diseñar un modelo a escala de un vehículo de tracción de cuatro ruedas utilizando servomotores [Dynamixel](http://www.dynamixel.com).
+Con este proyecto se busca diseñar a escala el vehículo eléctrico proyecto ETadeo Car, realizado por la universidad de Bogotá Jorge Tadeo Lozano. Este vehículo corresponde a un modelo eléctrico con tracción de cuatro ruedas utilizando servomotores [Dynamixel](http://www.dynamixel.com). El desarrollo incluye las etapas de; diseño, construcción, prueba de motores, construcción del circuito teniendo en cuenta la posibilidad de control mediante bluetooth y programación del controlador integrando la cinemática del robot para relacionarlo con velocidad lineal y angular de entrada.
+
+## Industrial robotics 2021-1
+This project seeks to scale the electric vehicle ETadeo Car project, carried out by the University of Bogotá Jorge Tadeo Lozano. This vehicle corresponds to an electric model with four-wheel drive using [Dynamixel](http://www.dynamixel.com) servo motors. Development includes the stages of; design, construction, motor testing, circuit construction taking into account the possibility of bluetooth control and controller programming integrating the kinematics of the robot to relate it to linear and angular input velocity.
+
+
 ### Construccción
-Se hizo la estructura con riel DIN sobre el que se pusieron los servomotores:
-![Estructura](images/m1.jpeg)
+Teniendo en cuenta el diseño determinado para lograr una escala aproximada en la estructura de 1:4, son empleados elementos de la marca robotis para la construcción de cada una de los grupos móviles de joint y wheel.
+Como elemento chasis se tiene el uso de riel DIN, con el fin de lograr una estructura rígida, reduciendo el peso y con ventajas para el ensamble rápido y ajustes ante cambios que puedan presentarse en el diseño.  Es importante tener en cuenta el acople entre cables para lograr la extensión que permita alcanzar la distancia parámetro entre la base de las ruedas. Sin embargo, es fundamental observar la forma de entrada en los motores, ya que estos no permiten adaptabilidad a modelos que se puedan encontrar en el mercado genérico. Esto lo convierte en un recurso limitante en la construcción del modelo a escala junto con los tornillos que permiten la unión de las partes.
+
+![Estructura eTadeoCar](images/estructura.jpeg)
+
+Antes de hacer la unión de los grupos de joint y wheel al chasis, se realizaron pruebas con los motores en el software Dynamixel Wizard 2.0 con la utilización de el hardware de robotis USB2Dynamixel, quien precisa de un controlador en el computador para su uso efectivo. En este se caracterizaron los motores según el movimiento que realizan (joint o wheel), se configuraron las ID para utilizar en la programación y los baudios de comunicación, los cuales son adaptados en 1000000.
+
+![Dynamixel wheel](images/test_wheel.gif)
+![Dynamixel joint](images/test_joint.gif)
 
 ### URDF
 Se modeló el robot utilzando URDF dentro del entorno de ROS
-![Estructura](images/urdf.png)
+![urdf](images/urdf.png)
 
 Del cual se generó la cadena cinemática
-![Estructura](images/chain.png)
+![kinematic chain](images/chain.png)
 
 ### Circuito
-Se diseñó el circuito utilizando Proteus;
-![Estructura](circuito/circuitoRobot1.png)
+Los componentes inmersos en el circuito que permite el funcionamiento del modelo son; controlador Arduino Mega 2560. Integrado SN74lS241N, el cual permite la comunicación entre el controlador y los motores al ser un buffer tri-estado. Módulo bluetooth HC-06 para el control remoto del modelo a escala. Fuente de poder que permita la alimentación de los motores dynamixel. (FotoCircuito)
+
+El funcionamiento de este circuito puede describirse desde la potencia de entrada, para esto fue empleada la fuente que nos permite tener a la salida 12V a 5A DC, siendo adecuada para la alimentación de los motores dynamixel. Es importante destacar el negativo común para el controlador y el integrado. Adicional a esto, se debe tener en cuenta el orden de los pines para iniciar la serie que alimenta los motores 
+
+![Dynamixel pinout](https://os.mbed.com/media/components/pinouts/ax12pinout.png)
+
+La conexión del circuito integrado nos permite tener la comunicación entre el controlador Arduino Mega 2560 y los motores Dynamixel, debido a el tratamiento que este le da a los datos para permitir figurar como transmisor y receptor. En el caso descrito se utiliza el pin 2 de Arduino como pin de control y los pines 18 y 19, correspondientes a TX1 y RX1 correspondientemente. Se deben tener en cuenta conexiones propias del circuito para su funcionamiento Para el funcionamiento del módulo bluetooth realizamos la conexión de TXD del HC-06 al pin 15 de la placa Arduino correspondiente al RX3. Por su parte, el Pin RXD está conectado al pin 14 de la placa Arduino correspondiente a TX3.
+El voltaje positivo de 5v para la alimentación de la placa Arduino se obtiene de la fuente brindada por el computador. La alimentación del circuito integrado y el módulo bluetooth es obtenido de la fuente del Arduino, utilizando su salida de 5v. No olvidar que es común el valor correspondiente a  0V. Se encuentra contenido en el repositorio el circuito impreso.
+
+Las conexiones en protoboard del circuito se presentan a continuación junto con el diseño realizado en Proteus.
+
+![Circuit proteus](circuito/circuitoRobot1.png)
+![Protoboard](circuito/proto.jpeg)
+
+### Conexión bluetooth
+Con el uso de la aplicación bluetooth electronics descargada en Android, se permite la realización de un panel para control del modelo. Esta aplicación se conecta de forma sencilla con el módulo para iniciar la transmisión de datos.
+Como se aprecia en la imágen, se utiliza un pad de 4 posiciones para enviar el carácter deseado que se procesa en el código para su ejecución en los motores Dynamixel. También se cuenta con un botón de parada, útil en cualquier momento ante algún imprevisto que se pueda presentar durante la ejecución.
 
 ### Código
-Se programaron los servomotores utilizando [Arduino](https://www.arduino.cc)
+Se programaron los servomotores utilizando [Arduino](https://www.arduino.cc) con la librería `Dynamixel.h`, las funciones creadas para desarrollar toda la lógica de funcionamiento fueron:
+
+```
+/**
+ * Calcula la velocidad que debe tener cada rueda
+*/
+void computeVel()
+```
+```
+/**
+ * Calculan el ángulo de cada joint
+*/
+void computeAng()
+void computeAngAux()
+```
+```
+/**
+ * Retorna el sentido de giro de la rueda
+ * Toma de parámetro la velocidad
+*/
+int selectSide(float vel)
+```
+```
+/**
+ * Pone en movimiento cada una de las ruedas
+*/
+void runCar()
+```
+```
+/**
+ * Gira cada joint a la posición deseada
+*/
+void turn()
+```
+```
+/**
+ * Deja los joint en posiciión 0
+ * Detiene todos los motores
+*/
+void stopAll()
+```
+### Conexión Bluetooth
+
+Con el uso de la aplicación bluetooth electronics descargada en Android, se permite la realización de un panel para control del modelo. Esta aplicación se conecta de forma sencilla con el módulo para iniciar la transmisión de datos.
+Como se aprecia en la imágen, se utiliza un pad de 4 posiciones para enviar el carácter deseado que se procesa en el código para su ejecución en los motores Dynamixel. También se cuenta con un botón de parada, útil en cualquier momento ante algún imprevisto que se pueda presentar durante la ejecución. 
+
+![Bluetooth app](images/app_layout.jpeg)
--- ino/car.ino
+++ ino/car.ino
@@ -1,60 +1,70 @@
 #include <DynamixelSerial.h>
-#define M_PI_2      1.57079632679489661923
+#define M_PI 3.14159265358979323846
+#define M_PI_2 1.57079632679489661923
+
+int CAMBIO_VEL_LIN = 30;
+int CAMBIO_VEL_ANG = 3;
+int OFFSET_L_FRONT = 650;
+int OFFSET_R_FRONT = 0; //encontrar experimentalmente
+int OFFSET_L_REAR = 450;
+int OFFSET_R_REAR = 0; //encontrar experimentalmente
+
+
+/**
+ * Definición de los motores de dirección
+*/
+int dir_left_front = 11;
+int dir_right_front = 17;
+int dir_left_rear = 13;
+int dir_right_rear = 14;
+
+/**
+ * Definición de los motores de las ruedas
+*/
+int wheel_left_front = 1;
+int wheel_right_front = 2;
+int wheel_left_rear = 10;
+int wheel_right_rear = 3;
+
+/**
+ * Velocidades angular y lineal deseadas
+*/
 float ang_vel_des = 0;
 float lin_vel_des = 0;
+/**
+ * Velocidad que debe tomar cada motor de rueda
+*/
 float vel_left_front = 0;
 float vel_right_front = 0;
 float vel_left_rear = 0;
 float vel_right_rear = 0;
+
+/**
+ * Velocidad que debe tomar cada motor de dirección
+*/
 float front_left_steering = 0;
 float front_right_steering = 0;
 float rear_left_steering = 0;
 float rear_right_steering = 0;
 
-double track_ = 0;  //buscar la medida
-double radius_ = 0; //buscar la medida
-double base_ = 0;   //buscar la medida
-
-// fabs() - abs()
-// copysign() - copysign() preguntar!
+/**
+ * Medidas tomadas del vehículo
+*/
+double track_ = 27;
+double radius_ = 3;
+double base_ = 10;
 
 void setup()
 {
     Dynamixel.setSerial(&Serial1); // &Serial - Arduino UNO/NANO/MICRO, &Serial1, &Serial2, &Serial3 - Arduino Mega
-    Dynamixel.begin(1000000, 3);   // Inicialize the servo at 1 Mbps and Pin Control 2
+    Dynamixel.begin(1000000, 2);   // Inicialize the servo at 1 Mbps and Pin Control 2
+    Serial.begin(115200);
+    Serial3.begin(9600);
     delay(1000);
 }
-
-void loop()
-{
-    if(abs(2*lin_vel_des) > abs(ang_vel_des*track_)){
-        computeVel();
-    }
-    if(abs(2*lin_vel_des) > abs(ang_vel_des*track_)){
-        computeAng();
-    }
-    else if(abs(lin_vel_des) > 0.001){
-        preguntar();
-    }
-
-    Dynamixel.move(3, random(200, 800)); // Move the Servo radomly from 200 to 800
-    delay(1000);
-    Dynamixel.moveSpeed(3, random(200, 800), random(200, 800));
-    delay(2000);
-    Dynamixel.setEndless(3, ON);
-    Dynamixel.turn(3, RIGTH, 1000);
-    delay(3000);
-    Dynamixel.turn(3, LEFT, 1000);
-    delay(3000);
-    Dynamixel.setEndless(3, OFF);
-    Dynamixel.ledStatus(3, ON);
-    Dynamixel.moveRW(3, 512);
-    delay(1000);
-    Dynamixel.action();
-    Dynamixel.ledStatus(3, OFF);
-    delay(1000);
-}
-
+/**
+ * Calcula la velocidad que debe tener cada rueda
+*/
 void computeVel()
 {
     vel_left_front = copysign(1.0, lin_vel_des) * sqrt((pow(lin_vel_des - ang_vel_des * track_ / 2, 2) + pow(base_ * ang_vel_des / 2.0, 2))) / radius_;
@@ -63,18 +73,155 @@ void computeVel()
     vel_right_rear = copysign(1.0, lin_vel_des) * sqrt((pow(lin_vel_des + ang_vel_des * track_ / 2, 2) + pow(base_ * ang_vel_des / 2.0, 2))) / radius_;
 }
 
+/**
+ * Calcula el ángulo de cada joint
+*/
 void computeAng()
 {
-    front_left_steering = atan(ang_vel_des*base_ / 2.0*lin_vel_des - ang_vel_des*track_));
-    front_right_steering = atan(ang_vel_des * base_ / (2.0 * lin_vel_des + ang_vel_des * track_));
-    rear_left_steering = -atan(ang_vel_des * base_ / (2.0 * lin_vel_des - ang_vel_des * track_));
-    rear_right_steering = -atan(ang_vel_des * base_ / (2.0 * lin_vel_des + ang_vel_des * track_));
+    front_left_steering = atan2(ang_vel_des * base_, (2.0 * lin_vel_des - ang_vel_des * track_));
+    front_right_steering = atan2(ang_vel_des * base_, (2.0 * lin_vel_des + ang_vel_des * track_));
+    rear_left_steering = -atan2(ang_vel_des * base_, (2.0 * lin_vel_des - ang_vel_des * track_));
+    rear_right_steering = -atan2(ang_vel_des * base_, (2.0 * lin_vel_des + ang_vel_des * track_));
 }
 
-void preguntar()
+/**
+ * Calculan el ángulo de cada joint
+*/
+void computeAngAux()
 {
     front_left_steering = copysign(M_PI_2, ang_vel_des);
     front_right_steering = copysign(M_PI_2, ang_vel_des);
     rear_left_steering = copysign(M_PI_2, -ang_vel_des);
     rear_right_steering = copysign(M_PI_2, -ang_vel_des);
+}
+
+/**
+ * Retorna el sentido de giro de la rueda
+ * Toma de parámetro la velocidad
+*/
+int selectSide(float vel)
+{
+    return vel < 0;
+}
+
+/**
+ * Pone en movimiento cada una de las ruedas
+*/
+void runCar()
+{
+    Dynamixel.setEndless(wheel_left_front, ON);
+    Dynamixel.setEndless(wheel_right_front, ON);
+    Dynamixel.setEndless(wheel_left_rear, ON);
+    Dynamixel.setEndless(wheel_right_rear, ON);
+
+    int temp_vel_1 = map(abs(vel_left_front), 0, 50, 0, 1023); //cambiar ese 50vpor 150
+    int temp_vel_2 = map(abs(vel_right_front), 0, 50, 0, 1023);
+    int temp_vel_3 = map(abs(vel_left_rear), 0, 50, 0, 1023);
+    int temp_vel_4 = map(abs(vel_right_rear), 0, 50, 0, 1023);
+
+    Dynamixel.turn(wheel_left_front, selectSide(vel_left_front), temp_vel_1);
+    Dynamixel.turn(wheel_right_front, selectSide(vel_right_front), temp_vel_2);
+    Dynamixel.turn(wheel_left_rear, selectSide(vel_left_rear), temp_vel_3);
+    Dynamixel.turn(wheel_right_rear, selectSide(vel_right_rear), temp_vel_4);
+
+}
+
+/**
+ * Gira cada joint a la posición deseada
+*/
+void turn(){
+
+    int temp_ang_1 = map(front_left_steering * RAD_TO_DEG, -90, 90, 0, 306);
+    int temp_ang_2 = map(front_right_steering * RAD_TO_DEG, -90, 90, 0, 306);
+    int temp_ang_3 = map(rear_left_steering * RAD_TO_DEG, -90, 90, 0, 306);
+    int temp_ang_4 = map(rear_right_steering * RAD_TO_DEG, -90, 90, 0, 306);
+
+    if (front_left_steering  < 0) temp_ang_1 *= -1;
+    if (front_right_steering < 0) temp_ang_2 *= -1;
+    if (rear_left_steering   < 0) temp_ang_3 *= -1;
+    if (rear_right_steering  < 0) temp_ang_4 *= -1;
+
+    /*
+    *Comentar esta líne para hacer pruebas del sentido de giro*
+    */
+    Dynamixel.move(dir_left_front, temp_ang_1 + OFFSET_L_FRONT - 153); // izquierda v2 // ofset 650/1024
+    Dynamixel.move(dir_right_front, temp_ang_2 + OFFSET_R_FRONT - 153); // mi derecha   temp_ang_2
+    Dynamixel.move(dir_left_rear, temp_ang_3 + OFFSET_L_REAR - 153); // derecha v2 //offset 450/1024
+    Dynamixel.move(dir_right_rear, temp_ang_4 + OFFSET_R_REAR - 153); // mi izquierda     temp_ang_4
+    
+    /*
+    *Para observar mejor en las peruabs de sentido de giro*
+    */
+    // Dynamixel.move(dir_left_front, 0); // izquierda v2 // ofset 650/1024
+    // Dynamixel.move(dir_right_front, 0); // mi derecha   temp_ang_2
+    // Dynamixel.move(dir_left_rear, 1023); // derecha v2 //offset 450/1024
+    // Dynamixel.move(dir_right_rear, 0); // mi izquierda     temp_ang_4
+
+    // Serial.println("***ANGULOS***");
+    // Serial.println(front_left_steering * RAD_TO_DEG);
+    // Serial.println(front_right_steering * RAD_TO_DEG);
+    // Serial.println(rear_left_steering * RAD_TO_DEG);
+    // Serial.println(rear_right_steering * RAD_TO_DEG);
+}
+
+
+/**
+ * Deja los joint en posiciión 0
+ * Detiene todos los motores
+*/
+void stopAll()
+{
+    Dynamixel.setEndless(wheel_left_front, OFF);
+    Dynamixel.setEndless(wheel_right_front, OFF);
+    Dynamixel.setEndless(wheel_left_rear, OFF);
+    Dynamixel.setEndless(wheel_right_rear, OFF);
+
+    Dynamixel.move(dir_left_front, OFFSET_L_FRONT); //ENSAYAR
+    Dynamixel.move(dir_right_front, OFFSET_R_FRONT);
+    Dynamixel.move(dir_left_rear, OFFSET_L_REAR);
+    Dynamixel.move(dir_right_rear, OFFSET_R_REAR);
+    Dynamixel.move(wheel_left_front, 0); 
+    Dynamixel.move(wheel_right_front, 0);
+    Dynamixel.move(wheel_left_rear, 0);
+    Dynamixel.move(wheel_right_rear, 0);
+}
+
+void loop()
+{
+    unsigned char comando = 0;
+    if (Serial.available())
+    {
+        comando = Serial.read();
+        if (comando == 'a')
+            lin_vel_des += CAMBIO_VEL_LIN;
+        else if (comando == 'A')
+            lin_vel_des -= CAMBIO_VEL_LIN;
+        else if (comando == 'b')
+            ang_vel_des += CAMBIO_VEL_ANG;
+        else if (comando == 'B')
+            ang_vel_des -= CAMBIO_VEL_ANG;
+        else if (comando == 's' || comando == 'S')
+        {
+            lin_vel_des = 0;
+            ang_vel_des = 0;
+        }
+    }
+
+    if (abs(2 * lin_vel_des) > abs(ang_vel_des * track_))
+    {
+        computeVel();
+        runCar();
+    }
+    if (abs(2 * lin_vel_des) > abs(ang_vel_des * track_))
+    {
+        computeAng();
+        turn();
+    }
+    else if (abs(lin_vel_des) > 0.001)
+    {
+        computeAngAux();
+        turn();
+    }
+    else if (lin_vel_des == 0 && ang_vel_des == 0) stopAll();
+    delay(500);
 }
